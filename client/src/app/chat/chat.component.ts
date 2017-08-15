0206a0b8b06d0a37533167bcbc40af400dd9152c
davidmgisbert <davidmgisbert@outlook.com>
2017-08-15 20:20:40 +0200

    Add chat component
    HTML: Add basic structure and bind data
    CSS: Add styles to the chat
    TS: Add basic controller to send and show messages and to show users

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from "@angular/router";
 
 import * as io from 'socket.io-client';
+import * as moment from 'moment';
 
 @Component({
   selector: 'main-chat',
@@ -8,21 +11,68 @@ import * as io from 'socket.io-client';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
-  socket = null;
+  user: string;
+  socket: any = null;
+  chatForm: FormGroup;
+  messages: Array<Array<string>>;
+  logs: Array<Array<string>>;
+  users: Array<string>;
 
-  constructor() {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.user = this.getUser();
+    if (!this.user) {
+      this.router.navigateByUrl('login');
+      return
+    }
+
+    this.messages = [];
+    this.logs = [];
+    this.users = [];
+
     this.connect();
     this.setEvents();
+    this.chatForm = this.fb.group({
+      message: ['']
+    });
+  }
+
+  onSubmit({ value }: { value: any }) {
+    this.socket.emit('message', { user: this.user, message: value.message, date: moment().format('lll') });
+    this.chatForm.reset();
+  }
+
+  @HostListener('window:beforeunload')
+  disconnectFromChat() {
+    this.socket.emit('user disconnected', { user: this.user, message: 'User disconnected', date: moment().format('lll') });
+  }
+
+  getUser() {
+    return localStorage.getItem('user');
   }
 
   connect() {
     this.socket = io.connect('http://localhost:4201');
-    this.socket.emit('user added', { hello: 'World' });
+    this.socket.emit('user added', { user: this.user, message: 'User added', date: moment().format('lll') });
   }
 
   setEvents() {
-    this.socket.on('user added', function (data) {
-      console.log(data);
-    })
+    this.socket.on('message', (data) => {
+      this.messages.push([data.user, data.message, data.date]);
+    });
+
+    this.socket.on('user added', (data) => {
+      this.users = data.users;
+    });
+
+    this.socket.on('user disconnected', (data) => {
+      this.users = data.users;
+    });
   }
 }
