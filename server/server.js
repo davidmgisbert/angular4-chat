cb22913cd1564e838e78705a21b77988597732cc
davidmgisbert <davidmgisbert@outlook.com>
2017-08-15 20:19:29 +0200

    Add event for user disconnect

--- server/server.js
+++ server/server.js
@@ -8,8 +8,27 @@ app.get('/', function (req, res) {
   res.send('Server listening on port 4201');
 });
 
+var users = [];
+
 io.on('connection', function (socket) {
   socket.on('user added', function (data) {
+    users.push(data.user);
+    data.users = users;
+
+    socket.broadcast.emit('user added', data);
     socket.emit('user added', data);
+  });
+
+  socket.on('message', function (data) {
+    socket.broadcast.emit('message', data);
+    socket.emit('message', data);
+  });
+
+  socket.on('user disconnected', function (data) {
+    users.splice(users.indexOf(data.user), 1);
+    data.users = users;
+
+    socket.broadcast.emit('user disconnected', data);
+    socket.emit('user disconnected', data);
   });
 });
