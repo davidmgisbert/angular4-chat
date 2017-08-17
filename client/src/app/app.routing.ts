b7800008b776e00bd6ac365288615325bb4b3ebf
davidmgisbert <davidmgisbert@outlook.com>
2017-08-17 21:01:50 +0200

    Add CanActivate AuthGuard

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './_service/auth-guard.service';
+
 import { LogInComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ChatComponent } from './chat/chat.component';
@@ -10,7 +12,7 @@ import { PageNotFoundComponent } from './shared/page-not-found.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LogInComponent },
-  { path: 'chat', component: ChatComponent },
+  { path: 'chat', component: ChatComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
